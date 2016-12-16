61c018a443d4fcb916187a929b77223480513e00
mmaka <maka@fizyka.umk.pl>
2016-12-16 23:19:51 +0100

    Dodanie katalogu Obiekty

---
+++ Obiekty/Punkt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dodawanie_figur1.Obiekty
+{
+    public class Punkt
+    {
+        public int x;
+        public int y;
+
+        public Punkt(int i = 0, int j = 0) { x = i; y = j; }
+        public void wypisz()
+        {
+            Console.WriteLine("{0},{1}", x, y);
+        }
+    }
+}
