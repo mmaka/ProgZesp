2cad06d5b84093f3cd1c15a3f2315a517c317f5a
Jakub Reszka <kuba.net1@tlen.pl>
2016-12-25 18:20:11 +0100

    Szkielet nowego interfejsu
    Wbrew opinii o braku pracy wysyłam szkielet interfejsu, całkowicie nowy, oparty na plikach Michała.
    Tworzony w WPF.
    Rozmieszczanie juz działa.  Do zrobienia jeszcze dużo.

---
+++ Rozmieszczenie/Widoki/nowa_prostokat.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Rozmieszczenie.Logika;
+using System.Windows;
+
+namespace Rozmieszczenie.Widoki
+{
+    /// <summary>
+    /// Interaction logic for nowa_prostokat.xaml
+    /// </summary>
+    public partial class nowa_prostokat : Window
+    {
+        Jądro J;
+        public nowa_prostokat(Jądro j)
+        {
+            J = j;
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            J.dodaj_prostokat();
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Rozmieszczenie/Widoki/widok_matryca.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Rozmieszczenie.Logika;
+using System.Windows;
+
+namespace Rozmieszczenie.Widoki
+{
+    /// <summary>
+    /// Interaction logic for widok_matryca.xaml
+    /// </summary>
+    public partial class widok_matryca : Window
+    {
+        Jądro J;
+        public widok_matryca(Jądro j)
+        {
+            J = j;
+            InitializeComponent();
+            
+        }
+    }
+}
