da7d48da1637a25042c7d6bed0e2a74ec6d69ac2
dawidgrusz1995 <dawidgrusz1995@gmail.com>
2016-12-31 20:00:09 +0100

    Add files via upload

---
+++ ProgZesp/Rozmieszczenie/Rozmieszczenie/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Rozmieszczenie.Logika;
+using Rozmieszczenie.Widoki;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Rozmieszczenie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Jądro J;
+        Zła_MatrycaFigura zMF;
+        nowa_prostokat np;
+        public MainWindow()
+        {
+            J = new Jądro(this);
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            J = new Jądro(this);
+            J.nowy_prostokat();
+        }
+
+    
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            J.nowy_matryca();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            J.zamknij();
+            Close();
+        }
+        private void _Edytuj_Prostokąt_Click(object sender, RoutedEventArgs e)
+        {
+            var x1 = dataGrid.SelectedItem;
+            np = new nowa_prostokat(J, (Prostokat)x1);
+            np.Show();
+            dataGrid.DataContext = Jądro.lista_obiektow;
+            dataGrid.Items.Refresh();
+
+        }
+        private void _Usuń_Prostokąt_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+                J.usuń_prostokąt((Prostokat)dataGrid.SelectedItem);
+
+        }
+        private void button_rozmiesc_Click(object sender, RoutedEventArgs e)
+        {
+            if (Jądro.listaStworzonychMatryc.Count == 0)   //zabazpieczenia przed rozmieszczeniem figur bez matrycy
+                MessageBox.Show("Stwórz Matrycę!", "Brak Matrycy", MessageBoxButton.OK);
+            else
+            {
+
+
+                List<Prostokat> listaObiektówKtóreMieszcząSieNaMatrycach = new List<Prostokat>();
+                foreach (var item2 in Jądro.lista_obiektow)                             //sprawdzanie czy obiekt zmieści sie na matrycy, jesli tak dodajemy do listy
+                {
+                    foreach (var item in Jądro.listaStworzonychMatryc)
+                    {
+
+                        if (item2.W <= item.rozmiar_x && item2.H <= item.rozmiar_y)
+                            listaObiektówKtóreMieszcząSieNaMatrycach.Add(item2);
+
+                    }
+                }
+
+                if (listaObiektówKtóreMieszcząSieNaMatrycach.Count != Jądro.lista_obiektow.Count)  //sprawdzamy czy jakaś figura się nie mieści 
+                {
+                    List<int> listaIDdoUsunięcia = new List<int>();
+                    var zapytanie = Jądro.lista_obiektow.Except<Prostokat>(listaObiektówKtóreMieszcząSieNaMatrycach);
+
+                    foreach (var item in zapytanie) //szukanie i dodawanie iod figur które sie nie mieszcza
+                    {
+                        listaIDdoUsunięcia.Add(item.ID);
+                    }
+
+
+                    if (zapytanie.Count() != 0)// jesli lista nie jest pusta to znaczy ze sa takie fiugury i trzeba albo je usunac albo zmienic rozmiar matrycy
+                    {
+                        foreach (var item in listaIDdoUsunięcia)
+                        {
+                            zMF = new Zła_MatrycaFigura(item, J, "Figura " + item + " nie zmieści się na\n żadnej matrycy. Co chcesz zrobić?", this);
+                            zMF.ShowDialog();
+
+                        }
+
+                    }
+                }
+
+
+                J.miksuj();
+            }
+        }
+    }
+}
---
+++ ProgZesp/Rozmieszczenie/Rozmieszczenie/Obiekty/Matryca.cs
@@ -0,0 +1,60 @@
+﻿using Rozmieszczenie.Widoki;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Rozmieszczenie
+{
+    public class Matryca
+    {
+        //rozmiar matrycy
+        public int rozmiar_x;
+        public int rozmiar_y;
+        
+        //zajetosc z perspektywy x - tj. indeksy tablicy to x, a wartości to maksymalny y (dla danego x)
+        public int[] zajetosc_x;
+        
+        public Matryca() { }
+        public Matryca(int x, int y)
+        {
+            rozmiar_x = x;
+            rozmiar_y = y;
+            zajetosc_x = new int[x+1];
+       
+            for (int i = 0; i < zajetosc_x.Length; i++)
+                zajetosc_x[i] = rozmiar_y;
+            
+        }
+        public Matryca(nowa_matryca NP)
+        {
+
+            int tmp_szerokosc = Convert.ToInt32(NP.textBox_matryca_szerokość.Text);
+            int tmp_wysokosc = Convert.ToInt32(NP.textBox_matryca_wysokość.Text);
+            if (tmp_szerokosc != 0 && tmp_wysokosc != 0)
+            {
+                rozmiar_x = tmp_szerokosc;
+                rozmiar_y = tmp_wysokosc;
+            }
+            NP.Close();
+        }
+        public void EdytujMatrycę(int w, int h)
+        {
+            this.rozmiar_x = w;
+            this.rozmiar_y = h;
+            zajetosc_x = new int[w + 1];
+
+            for (int i = 0; i < zajetosc_x.Length; i++)
+                zajetosc_x[i] = rozmiar_y;
+
+        }
+
+        //NIE UŻYWAMY TEJ METODY: reinicjacja tablicy - po wygenerowaniu rozmieszczenia, reinicjujemy tablicę danej matrycy przed kolejnym użyciem (potrzebne do generowania wielu rozmieszczen
+        public void reinicjuj_tab_zajetosci() 
+        {
+            for (int i = 0; i < zajetosc_x.Length; i++)
+                zajetosc_x[i] = rozmiar_y;
+            
+        }
+    }
+}
