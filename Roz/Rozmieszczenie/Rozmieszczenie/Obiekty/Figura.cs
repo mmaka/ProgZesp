874aed97775c242962bdf5d82fd71db9363b0c3a
mmaka <maka@fizyka.umk.pl>
2017-01-02 15:22:18 +0100

    Aktualny stan projektu

---
+++ Roz/Rozmieszczenie/Rozmieszczenie/Obiekty/Figura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozmieszczenie
+{
+    public interface IFigura
+    {
+        void wprowadz_wymiary();
+        bool ustal_pozycje(Punkt p,Matryca m);
+        string przedstaw_sie();
+      //  void rysuj();
+    }
+}
