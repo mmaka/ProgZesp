43b517b9a49310671cc5806f7f5f8095a8689777
Jakub Reszka <kuba.net1@tlen.pl>
2017-02-12 15:57:19 +0100

    Stary interfejs Win Forms do wglądu
    Przesyłam stary interfejs Win Forms do wglądu

---
+++ STARY_INT/ProgramowanieZespolowe/Widoki/Form_matryca.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace dodawanie_figur1
+{
+    partial class Form_matryca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_matryca));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox_Dymek = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 237);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // richTextBox_Dymek
+            // 
+            this.richTextBox_Dymek.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox_Dymek.DetectUrls = false;
+            this.richTextBox_Dymek.HideSelection = false;
+            this.richTextBox_Dymek.Location = new System.Drawing.Point(12, 259);
+            this.richTextBox_Dymek.Multiline = false;
+            this.richTextBox_Dymek.Name = "richTextBox_Dymek";
+            this.richTextBox_Dymek.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox_Dymek.Size = new System.Drawing.Size(621, 23);
+            this.richTextBox_Dymek.TabIndex = 4;
+            this.richTextBox_Dymek.Text = "";
+            // 
+            // Form_matryca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(827, 290);
+            this.Controls.Add(this.richTextBox_Dymek);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Location = new System.Drawing.Point(439, 0);
+            this.Name = "Form_matryca";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "Form_matryca";
+            this.Load += new System.EventHandler(this.Form_matryca_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+       public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.RichTextBox richTextBox_Dymek;
+    }
+}
---
+++ STARY_INT/ProgramowanieZespolowe/Widoki/Form_matryca.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace dodawanie_figur1
+{
+    public partial class Form_matryca : Form
+    {
+        Akcje_widok AW;
+        bool czy_Przesuwać = false;
+        public Form_matryca(float X, float Y, Akcje_widok _aw)
+        {
+            InitializeComponent();
+            AW = _aw;
+            pictureBox1.Width = (int)X;
+            pictureBox1.Height = (int)Y;            
+            pictureBox1.Show();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+            if (e.Button == MouseButtons.Left)
+            {
+                Cursor.Current = Cursors.Hand;
+                czy_Przesuwać = true;
+                AW.klikniecie_ktora_figura(e);
+                AW.przemiesc(e);
+            }
+           
+        }
+
+        private void Form_matryca_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+        
+    }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (czy_Przesuwać)
+                AW.przemiesc(e);
+            else
+            {
+                
+                AW.Chmurka(e);
+            }
+
+        }
+
+        
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            czy_Przesuwać = false;
+            AW.unselect();
+
+        }
+
+        
+    }
+}
